19f9aaaecd548e3fe0d4bc1d3e55fe6fb9ee1e15
ngocsithp-lgtm <ngocsithp@gmail.com>
2026-09-24 15:59:03 +0700

    Fix missing verticalScroll import for online music scrolling

--- app/src/main/java/com/ngocsi/music/MainActivity.kt
+++ app/src/main/java/com/ngocsi/music/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
