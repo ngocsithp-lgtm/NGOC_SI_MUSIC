91b00fe284fd51626eb59b8a595aef893fb4c8e0
ngocsithp-lgtm <ngocsithp@gmail.com>
2026-09-24 12:29:47 +0700

    Fix missing Compose Dialog import

--- app/src/main/java/com/ngocsi/music/MainActivity.kt
+++ app/src/main/java/com/ngocsi/music/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.Image
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
