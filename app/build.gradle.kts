0926e3228a2cb5afde72c57ab97a4fc215d3a899
ngocsithp-lgtm <ngocsithp@gmail.com>
2026-09-25 10:59:06 +0700

    v147 bump app version for dedicated YouTube player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ngocsi.music"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.1"
+        versionCode = 4
+        versionName = "3.2"
         buildConfigField("String", "YOUTUBE_API_KEY", "\"$youtubeApiKey\"")
     }
 
