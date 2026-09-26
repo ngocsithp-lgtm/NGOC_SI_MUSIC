462f3704a18440378e31c5c53d4f887329228ea6
ngocsithp-lgtm <ngocsithp@gmail.com>
2026-09-26 11:17:23 +0700

    Upgrade Media3 to 1.9.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.webkit:webkit:1.17.1")
-    implementation("androidx.media3:media3-exoplayer:1.5.1")
-    implementation("androidx.media3:media3-session:1.5.1")
+    implementation("androidx.media3:media3-exoplayer:1.9.4")
+    implementation("androidx.media3:media3-session:1.9.4")
 }
