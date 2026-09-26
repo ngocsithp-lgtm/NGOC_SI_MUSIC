38ebc41de8a4e7eb9c2d97dc1f79785b7cc7a5ab
ngocsithp-lgtm <ngocsithp@gmail.com>
2026-09-26 20:43:59 +0700

    Bump NGOC SI MUSIC PRO to 5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ngocsi.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "5.0"
+        versionCode = 21
+        versionName = "5.1"
         buildConfigField("String", "YOUTUBE_API_KEY", "\"$youtubeApiKey\"")
     }
 
